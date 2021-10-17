90395dff9cdd994485839dd49a36478d4ccbf9bd
Juan <49132815+juanccassano@users.noreply.github.com>
2021-10-17 20:03:13 -0300

    Agregado script security

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const mercadopago = require("mercadopago");
 mercadopago.configure({
   access_token:
     "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398",
+  integrator_id: "dev_24c65fb163bf11ea96500242ac130004",
 });
 
 app.engine("handlebars", exphbs());
